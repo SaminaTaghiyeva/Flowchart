be58e3b475ca0a953a177d928b69e6f13655054a
Samina Taghiyeva <saminast@code.edu.az>
2021-03-14 20:21:32 +0400

    Homework

---
+++ Flowcharts.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Flowcharts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task1
+
+            int a = 3;
+        Begin:
+            if (a == 5)
+            {
+                Console.WriteLine("Carpe diem");
+            }
+            else
+            {
+                ++a;
+                goto Begin;
+            }
+
+            //Task2
+
+            string[] word = { "a", "l", "m", "a" };
+            if (word[0] == "a")
+            {
+                Console.WriteLine("Please, write a new word");
+            }
+            else
+            {
+                Console.WriteLine("alma");
+            }
+
+        //Task3
+
+        Begin:
+            int a = 3527;
+            if (a >= 1000 && a <= 9999)
+            {
+                Console.WriteLine(a / 100);
+            }
+            else
+            {
+                goto Begin;
+            }
+        }
+    }
+}
